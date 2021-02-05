ce84c69582f62891a7c044b1797827b1986c1e83
Rohguentak <rhtg78@gmail.com>
2021-02-05 06:53:59 +0000

    word value null error handling

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask("extendedScrapper")
 
@@ -9,6 +9,10 @@ def home():
 @app.route("/report")
 def report():
   word = request.args.get('word')
+  if word:
+    word = word.lower()
+  else:
+    return redirect("/")
   return render_template("report.html",searchby=word)
 
 #@app.route("/<username>")
