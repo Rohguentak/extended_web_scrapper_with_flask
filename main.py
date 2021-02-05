714e8bd0da5f488a30f01b52044ebe0f2d1f396f
Rohguentak <rhtg78@gmail.com>
2021-02-05 08:35:22 +0000

    make csv file and export file for download

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, send_file
 from scrapper import get_jobs
+from save_as_csv import save_to_file
+
+
 
 app = Flask("extendedScrapper")
 
@@ -24,6 +27,21 @@ def report():
     return redirect("/")
   
   return render_template("report.html",searchby=word,jobnum=len(job_result), jobs=job_result)
+
+@app.route("/export")
+def export():
+  try:
+    word = request.args.get('word')
+    if not word:
+      raise Exception()
+    word = word.lower()
+    jobs = db.get(word)
+    if not jobs:
+      raise Exception()
+    save_to_file(jobs)
+    return send_file("job_info.csv")
+  except:
+    return redirect("/")
 
 #@app.route("/<username>")
 #def contact(username):
---
+++ save_as_csv.py
@@ -0,0 +1,11 @@
+import csv
+
+
+def save_to_file(jobs):
+  file = open("job_info.csv", mode="w")
+  writer = csv.writer(file)
+  writer.writerow(["TITLE","COMPANY","LOCATION","Link"])
+  for job in jobs:
+    writer.writerow(list(job.values()))
+  print(jobs)
+  return
